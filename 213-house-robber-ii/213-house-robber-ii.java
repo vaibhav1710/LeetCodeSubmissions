34b89573f08c3fd09adf819a822b20af5f963209
Vaibhav Singh <singhvaibhav1710@gmail.com>
2022-04-11 14:59:46 +0530

    Time: 1 ms (30.09%), Space: 42.2 MB (15.04%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int rob(int[] nums) {
+         
+        if(nums.length==1){
+            return Math.max(0,nums[0]);
+        }
+        else if(nums.length==2){
+            return Math.max(nums[0],nums[1]);
+        }else{
+        int ans[][] = new int[nums.length][2];
+        ans[0][0] = nums[0];
+        ans[0][1] = 0;
+        int n = nums.length;
+        for(int i=1;i<nums.length-1;i++){
+            ans[i][0] = ans[i-1][1]+nums[i];
+            ans[i][1] = Math.max(ans[i-1][0],ans[i-1][1]);
+        }
+        int fans=0;
+        fans = Math.max(ans[n-2][0],ans[n-2][1]);
+        
+        ans = new int[n][2];
+        ans[1][0] = nums[1];
+        ans[1][1] = 0;
+         for(int i=2;i<nums.length;i++){
+            ans[i][0] = ans[i-1][1]+nums[i];
+            ans[i][1] = Math.max(ans[i-1][0],ans[i-1][1]);
+        }
+         fans =Math.max(fans , Math.max(ans[n-1][0],ans[n-1][1]));
+        
+        return fans;
+        }
+    }
+}
