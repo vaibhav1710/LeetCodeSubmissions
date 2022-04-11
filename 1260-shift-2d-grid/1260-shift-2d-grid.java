da14b81cfecd8708b3e7a2889d05043ad5c9e934
Vaibhav Singh <singhvaibhav1710@gmail.com>
2022-04-11 12:32:46 +0530

    Time: 7 ms (76.36%), Space: 54.8 MB (48.06%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,35 +1,22 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        
-        ArrayList<Integer> al = new ArrayList<>();
+     
+        List<List<Integer>> al = new ArrayList<>();
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                al.add(grid[i][j]);
-            }
+            al.add(new ArrayList<>());
+        }
+        int n = grid.length;
+        int m = grid[0].length;
+        int tot = n*m;
+        k = k%tot;
+        // m --> np. of column because , we can get (i,j) of any 
+        // index by using , i = id/m , j = id%m , even when 
+        // no. of rows != no. of coloumns
+        for(int i =0;i<tot;i++){
+            int id = (i+tot-k)%tot;
+            al.get(i/m).add(grid[id/m][id%m]);
         }
         
-        int a[] = new int[grid.length*grid[0].length];
-        int n  = a.length;
-        for(int i=0;i<al.size();i++){
-            int v = al.get(i);
-            a[(i+k)%n] = v;
-        }
-        
-        List<List<Integer>> al1 = new ArrayList<>();
-        n = grid[0].length;
-        
-        int m = grid.length;
-        int i=0;
-        while(m-->0){
-            al = new ArrayList<>();
-            n = grid[0].length;
-        while(n-->0){
-            al.add(a[i]);
-            i++;
-        }
-        al1.add(al);    
-        }
-           
-        return al1;
+        return al;
     }
 }
