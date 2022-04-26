8be78b36c2ef7b921b02d785eb4f249c4ee2b62f
Vaibhav Singh <singhvaibhav1710@gmail.com>
2022-04-27 02:46:19 +0530

    Time: 390 ms (48.73%), Space: 204.5 MB (6.51%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+     
+        ArrayList<Pair> [] al = new ArrayList[points.length];
+        for(int i=0;i<points.length;i++){
+            al[i] = new ArrayList<>();
+        }
+        for(int i=0;i<points.length;i++){
+            for(int j=i+1;j<points.length;j++){
+    al[i].add(new Pair(j,Math.abs(points[i][0]-points[j][0]) + Math.abs(points[j][1] - points[i][1])));
+                 al[j].add(new Pair(i,Math.abs(points[i][0]-points[j][0]) + Math.abs(points[j][1] - points[i][1])));
+            }
+        }
+        
+      int ans =  bfs(al , points);
+        return ans;
+        
+    }
+    
+    public int bfs( ArrayList<Pair> [] al,int[][] points){
+        int ans=0;
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+        q.add(new Pair(0,0));
+        boolean vis [] = new boolean[points.length];
+       int edge=0;
+        
+        while(q.size()>0 && edge!=points.length){
+            
+            Pair r = q.remove();
+            if(vis[r.x]){
+                continue;
+            }else{
+            for(Pair a:al[r.x]){
+                q.add(a);
+            }
+             vis[r.x] = true;
+             ans += r.wt;
+                edge++;
+            }    
+        }
+        return ans;
+    }
+    
+    class Pair implements Comparable<Pair>{
+        int x;
+        int wt;
+        Pair(int x,int wt){
+            this.x= x;
+            this.wt = wt;
+        }
+        public int compareTo(Pair o){
+            return this.wt-o.wt;
+        }
+    }
+    
+}
