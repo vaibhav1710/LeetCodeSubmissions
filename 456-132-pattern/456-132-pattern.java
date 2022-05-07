94836b997fdc3431cd03cf81029fd5d8a3195400
Vaibhav Singh <singhvaibhav1710@gmail.com>
2022-05-07 12:06:23 +0530

    Time: 36 ms (53.05%), Space: 62.2 MB (75.82%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-       
+        
+        // can1 < can2 > can3
+        // can3 > can1
+        
         int min[] = new int[nums.length];
         int n = nums.length;
         min[0] = nums[0];
+        // storing min for each index (which will be our can1)
         for(int i=1;i<n;i++){
             min[i] = Math.min(min[i-1],nums[i]);
         }
@@ -12,11 +16,18 @@ class Solution {
         st.push(nums[nums.length-1]);
         for(int i = n-2;i>0;i--){
             
-            if(nums[i]>min[i]){
+            // nums[i] is our can2
+            // st.peek() will be our can3
+            if(nums[i]>min[i]){  // can2 > can2
+                 // can3 > can1
+                
+                // removing all peek which are less than can1
                 while(st.size()>0 && st.peek()<=min[i]){
                     st.pop();
                 }
-                
+             
+                // since can1 < can3
+                // Now for answer to exist can3 < can2
                 if(st.size()>0 && st.peek()<nums[i]){
                     return true;
                 }else{
