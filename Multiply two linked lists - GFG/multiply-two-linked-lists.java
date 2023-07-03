0894975cb892f3d9943e76f2a23b0032a3dba93f
Vaibhav Singh <singhvaibhav1710@gmail.com>
2023-07-03 15:06:04 +0530

    Added solution - LeetHub

---
+++ Multiply two linked lists - GFG/multiply-two-linked-lists.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	
+// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+         long v1=0;
+         long v2=0;
+         long mod = 1000000007;
+         while(l1!=null){
+             v1 *= 10;
+             v1 += l1.data;
+             v1 %= mod;
+             l1 = l1.next;
+         }
+         while(l2!=null){
+             v2 *= 10;
+             v2 += l2.data;
+             v2 %= mod;
+             l2 = l2.next;
+         }
+         
+         return (v1*v2)%mod;
+   }
+}
