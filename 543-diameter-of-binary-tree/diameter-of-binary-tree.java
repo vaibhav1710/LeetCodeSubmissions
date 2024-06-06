9ecbc06ece48f16d7e11d3d63646e285eaede412
Vaibhav Singh <singhvaibhav1710@gmail.com>
2024-06-07 03:01:45 +0530

    Time: 1 ms (32.21%) | Memory: 44.3 MB (85.74%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,47 +13,23 @@
  *     }
  * }
  */
-
 class Solution {
     
-     class Pair{
+    class Pair{
      int dia;
-    int ht;
-     
+     int ht;
     }
-   
+    
     public int diameterOfBinaryTree(TreeNode root) {
-         
-        if(root==null){
-            return 0;
-        }
+        if(root.left==null && root.right==null) return 0;
         
-      //   int left = diameterOfBinaryTree(root.left);
-     //   int right = diameterOfBinaryTree(root.right);
+        Pair ans = solve(root);
+        return ans.dia;
         
-           //      int dia = Math.max(Math.max(left,right),height(root.left)+height(root.right)+2);  
-      //  return dia;
         
-        Pair res = dia2(root);
-        return res.dia;
     }
-    
- /*   public int h = -1;
-    public int height(TreeNode root){
-        if(root==null){
-            return -1;
-        }
-        
-        int left = height(root.left);
-        int right = height(root.right);
-        
-           h = Math.max(left,right)+1;
-        
-        return h;
-    }
- */   
-    
-    public Pair dia2(TreeNode root){
+     
+    public Pair solve(TreeNode root){
         if(root==null){
             Pair bp=new Pair();
             bp.dia = 0;
@@ -61,14 +37,14 @@ class Solution {
             return bp;
         }
         
-        Pair lf = dia2(root.left);
-        Pair rf = dia2(root.right);
+        Pair lf = solve(root.left);
+        Pair rf = solve(root.right);
         
         Pair mp = new Pair();
         
         mp.dia = Math.max(lf.dia,Math.max( rf.dia, lf.ht + rf.ht + 2));
         mp.ht = Math.max(lf.ht,rf.ht)+1;
-        
         return mp;
     }
+    
 }
