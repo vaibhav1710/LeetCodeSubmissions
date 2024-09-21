bdc18f3a1ce8fcfa0bc5d456d83de765167b9cc7
Vaibhav Singh <singhvaibhav1710@gmail.com>
2024-09-21 12:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (59.52%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,56 @@
 class Solution {
     public void nextPermutation(int[] arr) {
-        int n = arr.length;
+       /*
+        1 2 3 4 5
+        1 2 3 5 4
+        
+        1 2 4 3 5
+                        
+        1 2 4 5 3
+        
+        1 2 5 3 4
+       */
+        
         int ind = -1;
+        int n = arr.length;
         for(int i=n-2;i>=0;i--){
             if(arr[i]<arr[i+1]){
-                ind=i;
+                ind = i;
                 break;
             }
         }
+      //  System.out.println(ind);
         if(ind==-1){
             Arrays.sort(arr);
         }else{
-        int min = Integer.MAX_VALUE;
-        int ind1=-1;  
-        for(int i=n-1;i>ind;i--){
-            if(arr[i]>arr[ind] && arr[i]<min){
-                min = arr[i];
-                ind1=i;
+        
+        int max = Integer.MAX_VALUE;
+        int j = -1;
+        for(int i = ind+1;i<n;i++){
+            if(arr[i]>arr[ind]){
+                if(arr[i]<=max){
+                    max = arr[i];
+                    j = i;
+                }
             }
         }
-
-        int temp = arr[ind1];
-        arr[ind1] = arr[ind];
-        arr[ind] = temp;
-
-
-        int i=ind+1;
-        int j=n-1;
-    while(i<j){
-         temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
+        //System.out.println(j);
+        int v = arr[j];
+        arr[j] = arr[ind];
+        arr[ind] = v;
+        
+         int i = ind+1;
+         j = n-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        
+        }
+        
+        
     }
-}
-  }
 }
